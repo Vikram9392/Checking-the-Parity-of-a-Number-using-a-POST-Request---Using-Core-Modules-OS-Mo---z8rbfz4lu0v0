251ab53d53dc4cc342fbf138017622b8bf6d6f65
Vikram9392 <148332548+Vikram9392@users.noreply.github.com>
2024-03-06 12:31:56 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -12,7 +12,20 @@ const server = http.createServer((req, res) => {
       const value = obj.num1;
     
      // Write the code here to check if the number is odd or even
-
+     if(parseInt(value)<0)
+     {
+       res.writeHead(404,{'Content-Type': 'text/plain'});
+       res.end("The operation cannot be performed");
+     }
+     if(parseInt(value)%2==0)
+     {
+        res.writeHead(200,{'Content-Type': 'text/plain'});
+        res.end(`The number ${value} is even`);
+     }
+     if(parseInt(value)%2==1){
+        res.writeHead(404,{'Content-Type': 'text/plain'});
+        res.end(`The number ${value} is odd`);
+     }
    });
   }
 
